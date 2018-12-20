ea3219b818cb17771991f5243e84d54a334d64ac
Wojciech Pirog <wojciech.pirog@polcode.net>
2018-12-20 14:32:51 +0100

    countries table migration

---
+++ database/migrations/2018_12_20_131734_create_countries_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name', 50);
+            $table->string('code', 2);
+            $table->timestamps();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
